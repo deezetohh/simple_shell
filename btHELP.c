1fdf4f9f708c19f5a15fe82f32d28c8460201f97
pbliphadzi <pbliphadzi@gmail.com>
2023-05-27 15:52:39 +0000

    debug

--- btHELP.c
+++ btHELP.c
@@ -5,35 +5,33 @@
 */
 int btHelp(char **args __attribute__((unused)), char *K __attribute__((unused)))
 {
-    const char *message[] = {
-     "Welcome to the  btshell!\n",
+    int i;
+    i = 0; 
+     const char *message[] = {
+        "Welcome to the btshell!\n",
         "Available commands:\n",
         "1. cd: Change directory\n",
         "2. help: Display available commands\n",
         "3. exit: Exit the shell\n",
         "4. env: Display environment variables\n",
         "5. ls: List files and directories\n",
-        "Thank you!\n"      
+        "Thank you!\n"
     };
 
     size_t lenmsg[] = {
-        bt_lstring(message[0]),
-        bt_lstring(message[1]),
-        bt_lstring(message[2]),
-        bt_lstring(message[3]),
-        bt_lstring(message[4]),
-        bt_lstring(message[5]),
-        bt_lstring(message[6]),
-        bt_lstring(message[7])
+        sizeof("Welcome to the btshell!\n"),
+        sizeof("Available commands:\n"),
+        sizeof("1. cd: Change directory\n"),
+        sizeof("2. help: Display available commands\n"),
+        sizeof("3. exit: Exit the shell\n"),
+        sizeof("4. env: Display environment variables\n"),
+        sizeof("5. ls: List files and directories\n"),
+        sizeof("Thank you!\n")
     };
-    write(STDOUT_FILENO, message[0], lenmsg[0]);
-    write(STDOUT_FILENO, message[1], lenmsg[1]);
-    write(STDOUT_FILENO, message[2], lenmsg[2]);
-    write(STDOUT_FILENO, message[3], lenmsg[3]);
-    write(STDOUT_FILENO, message[4], lenmsg[4]);
-    write(STDOUT_FILENO, message[5], lenmsg[5]);
-    write(STDOUT_FILENO, message[6], lenmsg[6]);
-    write(STDOUT_FILENO, message[7], lenmsg[7]);
 
+    for (i = 0; i < (sizeof(message) / sizeof(message[0])); i++)
+    {
+        write(STDOUT_FILENO, message[i], lenmsg[i]);
+    }
 return(1);
 }
